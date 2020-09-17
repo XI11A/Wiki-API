6871adc48fc1dc46f6dbab81b88920b0161046e6
Nivash <65842454+XI11A@users.noreply.github.com>
2020-09-17 18:28:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,26 @@
 from flask import Flask, jsonify, request
 import wikipedia
+app = Flask(__name__)
+app.url_map.strict.slashes = False
 
-app = Flask(__name )
-app.url_map.strict _slashes = False
+def get_wiki_data(search_query):
+    got_query = (wikipedia.search(search_query)[0])
+    page = wikipedia.page(got_query)
+    title = page.title
+    wiki_url = page.url_map
+    summary = (wikipedia,summary(got_query, sentences=1))
+    return title,wiki_url,summary
 
-def get wiki data(search query):
-got_query = (wikipedia.search(search query)[0])
-page = wikipedia.page(got query)
-title = page.title
-wiki url = page.url
-summary = (wikipedia.summary(got query, sentences=1))
-return title,wiki_url,summary
+    @app.route("/")
+    def index():
+        return "API is UP and working fine"
 
-@app.route('/')
-def index(): I
-return "API is UP and working fine"
+    @app.route('/api')
+    def home():
+        query = (request.args.get('query'))
+        title, url, summary = get_wiki_data(query)
+        return jsonify({"query":query,"title":title,"wiki_url";url,"summary":summary})
 
-@app.route('/<query>")
-def home(query):
-title, url, summary = get wiki data(query)
-return jsonify({"query":query,“"title":title, "wiki url”:url, "summary":summary})
-
-if _name_ == ' main_':
-app.debug=True
-app.run(host='0.0.0.0"',port=5000)
+    if __name__ == '__main__':
+        app.debug=True
+        app.run(host='0.0.0.0',port=5000)        
