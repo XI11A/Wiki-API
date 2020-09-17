8f93dea958d29e45d94b9630613aa179fefba952
Nivash <65842454+XI11A@users.noreply.github.com>
2020-09-17 17:31:21 +0530

    Initial Push

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import wikipedia
+app = Flask(__name__)
+app.url_map.strict.slashes = False
+
+def get_wiki_data(search_query):
+    got_query = (wikipedia.search(search_query)[0])
+    page = wikipedia.page(got_query)
+    title = page.title
+    wiki_url = page.url_map
+    summary = (wikipedia,summary(got_query, sentences=1))
+    return title,wiki_url,summary
+
+    @app.route("/")
+    def index():
+        return "API is UP and working fine"
+
+    @app.route('/api')
+    def home():
+        query = (request.args.get('query'))
+        title, url, summary = get_wiki_data(query)
+        return jsonify({"query":query,"title":title,"wiki_url";url,"summary":summary})
+
+    if __name__ == '__main__':
+        app.debug=True
+        app.run(host='0.0.0.0',port=5000)        
